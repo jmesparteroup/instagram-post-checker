ca7fee1651d6ec67442bbd29a81fed6219b25d60
Joshua Espartero <jammespartero@gmail.com>
2025-06-23 18:35:08 +0800

    Add monitoring script to layout for performance tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Metadata } from "next";
+import Script from "next/script";
 import QueryProvider from "@/components/providers/query-provider";
 
 const geistSans = Geist({
@@ -25,6 +26,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          defer
+          data-domain="insta-checker.jamme.dev"
+          src="https://monitoring.jamme.dev/js/script.js"
+          strategy="afterInteractive"
+        />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <QueryProvider>{children}</QueryProvider>
       </body>
